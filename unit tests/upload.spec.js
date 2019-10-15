e4470b8cf0487a5f81c71341d94415abf10aa15c
hortonr6@coventry.ac.uk <hortonr6@coventry.ac.uk>
2019-10-15 18:19:20 +0100

    Updated file upload to clean-up files

--- unit tests/upload.spec.js
+++ unit tests/upload.spec.js
@@ -4,17 +4,23 @@ const fs = require('fs') // Replace this and all fs function calls with mock of 
 const Upload = require('../modules/upload.js')
 
 describe('uploadFile()', () => {
-	test('verify that a file is copied to the correct location', async done => {
+	test('file is uploaded to the server', async done => {
 		expect.assertions(1)
 		const upload = await new Upload()
+
 		// Create dummy file
 		const text = 'This is a file to test the uploadFile function'
-		fs.writeFileSync('testing/dummy.txt', text)
+		await fs.writeFile('testing/dummy.txt', text, err => {
+			if (err) throw err
+		})
+
 		// Upload
 		upload.uploadFile('testing/dummy.txt', 'dummy.txt', 'testing')
+
 		// Check upload success
 		const existing = fs.existsSync('testing/dummy.txt')
 		expect(existing).toBeTruthy()
+
 		// Delete dummy file from uploads and from test folder
 		await fs.unlink('testing/dummy.txt', err => {
 			if (err) throw err
@@ -22,6 +28,13 @@ describe('uploadFile()', () => {
 		await fs.unlink('files/uploads/testing/dummy.txt', err => {
 			if (err) throw err
 		})
+		await fs.rmdir('files/uploads/testing', err => {
+			if (err) throw err
+		})
+
+		done() // Finish the test
+	})
+
 		done()
 	})
 })
