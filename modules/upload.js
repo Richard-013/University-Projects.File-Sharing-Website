fd7d20a63edb993b9a4feb9ffcf75b85d2a148c7
hortonr6 <hortonr6@coventry.ac.uk>
2019-10-15 15:45:30 +0100

    Made uploadFile function

--- modules/upload.js
+++ modules/upload.js
@@ -1,9 +1,18 @@
 'use strict'
+const fs = require('fs-extra')
 
 module.exports = class Upload {
 
-	async uploadFile() {
-		// Get file from form here
+	async uploadFile(path, name, user) {
+		if (fs.existsSync('files') !== true) {
+			fs.mkdirSync(`files/uploads/${user}`, { recursive: true })
+		} else if (fs.existsSync('files/uploads') !== true) {
+			fs.mkdirSync(`files/uploads/${user}`, { recursive: true })
+		} else if (fs.existsSync(`files/uploads/${user}`) !== true) {
+			fs.mkdirSync(`files/uploads/${user}`)
+		}
+
+		fs.copy(path, `files/uploads/${user}/${name}`)
 	}
 
 }
