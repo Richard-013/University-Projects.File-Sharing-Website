9984580c9e4abf22a14f3bd01db4b5774e000cd1
hortonr6@coventry.ac.uk <hortonr6@coventry.ac.uk>
2019-10-15 18:20:03 +0100

    Test for if file path directory is created

--- unit tests/upload.spec.js
+++ unit tests/upload.spec.js
@@ -35,6 +35,41 @@ describe('uploadFile()', () => {
 		done() // Finish the test
 	})
 
+	test('directory path is created if it does not exist', async done => {
+		expect.assertions(2)
+		const upload = await new Upload()
+
+		// Creates file to use as dummy upload
+		const text = 'This is a file to test the uploadFile function'
+		await fs.writeFile('testing/dummy.txt', text, err => {
+			if (err) throw err
+		})
+
+		// Checks that the folder does not exist
+		if (fs.existsSync('files/uploads/testing')) {
+			fs.rmdirSync('files/uploads/testing')
+		}
+
+		expect(fs.existsSync('files/uploads/testing')).toBeFalsy()
+
+		// Upload file to directory and check directory was created
+		upload.uploadFile('testing/dummy.txt', 'dummy.txt', 'testing')
+		expect(fs.existsSync('files/uploads/testing')).toBeTruthy() // Checks that the folder was created successfully
+
+		// Deletes files after test
+		await fs.unlink('testing/dummy.txt', err => {
+			if (err) throw err
+		})
+		await fs.unlink('files/uploads/testing/dummy.txt', err => {
+			if (err) throw err
+		})
+		await fs.rmdir('files/uploads/testing', err => {
+			if (err) throw err
+		})
+
+		done()
+	})
+
 		done()
 	})
 })
