48190c054ddbb181a8a2395aa12305f13fa5fa09
hortonr6@coventry.ac.uk <hortonr6@coventry.ac.uk>
2019-10-15 18:20:45 +0100

    Tidied uploadFile

--- modules/upload.js
+++ modules/upload.js
@@ -4,12 +4,9 @@ const fs = require('fs-extra')
 module.exports = class Upload {
 
 	async uploadFile(path, name, user) {
-		if (fs.existsSync('files') !== true) {
+		const pathExists = fs.existsSync(`files/uploads/${user}`)
+		if (pathExists !== true) {
 			fs.mkdirSync(`files/uploads/${user}`, { recursive: true })
-		} else if (fs.existsSync('files/uploads') !== true) {
-			fs.mkdirSync(`files/uploads/${user}`, { recursive: true })
-		} else if (fs.existsSync(`files/uploads/${user}`) !== true) {
-			fs.mkdirSync(`files/uploads/${user}`)
 		}
 
 		fs.copy(path, `files/uploads/${user}/${name}`)
