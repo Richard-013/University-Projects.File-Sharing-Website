d66244635456907315a69265247befc056e470eb
hortonr6 <hortonr6@coventry.ac.uk>
2019-10-15 15:39:23 +0100

    Rewrote test to align with new plan for function

--- unit tests/upload.spec.js
+++ unit tests/upload.spec.js
@@ -1,13 +1,27 @@
 'use strict'
 
+const fs = require('fs') // Replace this and all fs function calls with mock of fs
 const Upload = require('../modules/upload.js')
 
 describe('uploadFile()', () => {
-	test('verify that a file is received from user', async done => {
+	test('verify that a file is copied to the correct location', async done => {
 		expect.assertions(1)
 		const upload = await new Upload()
-		const file = upload.uploadFile()
-		expect(file.length).toBe(1)
+		// Create dummy file
+		const text = 'This is a file to test the uploadFile function'
+		fs.writeFileSync('testing/dummy.txt', text)
+		// Upload
+		upload.uploadFile('testing/dummy.txt', 'dummy.txt', 'testing')
+		// Check upload success
+		const existing = fs.existsSync('testing/dummy.txt')
+		expect(existing).toBeTruthy()
+		// Delete dummy file from uploads and from test folder
+		await fs.unlink('testing/dummy.txt', err => {
+			if (err) throw err
+		})
+		await fs.unlink('files/uploads/testing/dummy.txt', err => {
+			if (err) throw err
+		})
 		done()
 	})
 })
