9527e9ae4f2b6fb7a38edb14555057fb19563918
hortonr6@coventry.ac.uk <hortonr6@coventry.ac.uk>
2019-10-17 17:18:47 +0100

    Added tests for return value

--- unit tests/upload.spec.js
+++ unit tests/upload.spec.js
@@ -19,21 +19,23 @@ describe('uploadFile()', () => {
 	})
 
 	test('file is uploaded to the server', async done => {
-		expect.assertions(1)
+		expect.assertions(2)
 		const upload = await new Upload()
 
 		// Upload
-		await upload.uploadFile('testing/dummy.txt', 'dummy.txt', 'testing')
+		const returnVal = await upload.uploadFile('testing/dummy.txt', 'dummy.txt', 'testing')
 
 		// Check upload success
 		const existing = fs.existsSync('files/uploads/testing/dummy.txt')
 		expect(existing).toBeTruthy()
+		// Checks return value was correct
+		expect(returnVal).toBe(0)
 
 		done() // Finish the test
 	})
 
 	test('directory path is created if it does not exist', async done => {
-		expect.assertions(2)
+		expect.assertions(3)
 		const upload = await new Upload()
 
 		// Checks that the folder does not exist
@@ -44,14 +46,15 @@ describe('uploadFile()', () => {
 		expect(fs.existsSync('files/uploads/testing')).toBeFalsy()
 
 		// Upload file to directory and check directory was created
-		await upload.uploadFile('testing/dummy.txt', 'dummy.txt', 'testing')
+		const returnVal = await upload.uploadFile('testing/dummy.txt', 'dummy.txt', 'testing')
 		expect(fs.existsSync('files/uploads/testing')).toBeTruthy() // Checks that the folder was created successfully
-
+		// Checks return value was correct
+		expect(returnVal).toBe(0)
 		done()
 	})
 
 	test('directory path is not created if it already exists', async done => {
-		expect.assertions(3)
+		expect.assertions(4)
 		const upload = await new Upload()
 
 		if (fs.existsSync('files/uploads/testing') === false) {
@@ -62,13 +65,14 @@ describe('uploadFile()', () => {
 		expect(fs.existsSync('files/uploads/testing')).toBeTruthy()
 
 		// Upload file to directory
-		await upload.uploadFile('testing/dummy.txt', 'dummy.txt', 'testing')
+		const returnVal = await upload.uploadFile('testing/dummy.txt', 'dummy.txt', 'testing')
 
 		// Checks that the folder was not removed
 		expect(fs.existsSync('files/uploads/testing')).toBeTruthy()
 		// Checks that the folder was not created inside the existing directory
 		expect(fs.existsSync('files/uploads/testing/testing')).toBeFalsy()
-
+		// Checks return value was correct
+		expect(returnVal).toBe(0)
 		done()
 	})
 })
