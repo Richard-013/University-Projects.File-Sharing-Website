89b997f8bd574475fa8597261df0925f54d7d1c3
hortonr6@coventry.ac.uk <hortonr6@coventry.ac.uk>
2019-10-18 15:51:18 +0100

    Prevented directory being created for undefined user

--- modules/upload.js
+++ modules/upload.js
@@ -4,17 +4,17 @@ const fs = require('fs-extra')
 module.exports = class Upload {
 
 	async uploadFile(path, name, user) {
-		const pathExists = fs.existsSync(`files/uploads/${user}`)
-		if (pathExists !== true) {
-			fs.mkdirSync(`files/uploads/${user}`, { recursive: true })
-		}
-		
 		if (path === undefined || name === undefined) {
 			return 1 // No file or no path specified for upload
 		} else {
 			if(fs.existsSync(path) === false) {
 				return -1 // Selected file does not exist
 			} else {
+				const pathExists = fs.existsSync(`files/uploads/${user}`)
+				if (pathExists !== true) {
+					fs.mkdirSync(`files/uploads/${user}`, { recursive: true })
+				}
+				
 				await fs.copy(path, `files/uploads/${user}/${name}`)
 				return 0
 			}
