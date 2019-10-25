2be64e18f7be4e667d6a1d9e3506bf3f4cf230d2
hortonr6@coventry.ac.uk <hortonr6@coventry.ac.uk>
2019-10-25 22:12:55 +0100

    Fixed merge issues

--- index.js
+++ index.js
@@ -133,13 +133,10 @@ router.post('/upload', koaBody, async ctx => {
 			ctx.redirect('/upload?message=No file selected') // No file selected
 		} else if (uploadStatus === -1) {
 			ctx.redirect('/upload?message=Selected file does not exist') // File does not exist
-<<<<<<< HEAD
-=======
 		} else if (uploadStatus === -2) {
 			ctx.redirect('/upload?message=User has already uploaded a file with the same name') // User has uploaded file with the same name
 		} else if (uploadStatus === -3) {
 			ctx.redirect('/upload?message=Database error has occurred, please try again') // Database error encountered
->>>>>>> develop
 		} else {
 			ctx.redirect('/upload?message=Something went wrong') // Generic error
 		}
